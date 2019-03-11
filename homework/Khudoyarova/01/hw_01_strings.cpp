c34d156d024dcd425ff0863e28f10f020d6f5d0f
Anastasia Khudoyarova <asya-iriska@yandex.ru>
2019-03-11 17:26:59 +0300

    Added first hw. Khudoyarova Anastasia
    hw_01 - суммирование по столбцам
    hw_01 - исполняемый файл
    hw_01_strings - суммирование по строкам
    hw_01_strings - исполняемый файл

---
+++ homework/Khudoyarova/01/hw_01_strings.cpp
@@ -0,0 +1,47 @@
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+    using clock_t = std::chrono::high_resolution_clock;
+    using microseconds = std::chrono::microseconds;
+public:
+    Timer()
+        : start_(clock_t::now())
+    {
+    }
+
+    ~Timer()
+    {
+        const auto finish = clock_t::now();
+        const auto us = 
+            std::chrono::duration_cast<microseconds>
+                (finish - start_).count();
+        std::cout << us << " us" << std::endl;
+    }
+
+private:
+    const clock_t::time_point start_;
+};
+
+
+const int SIZE = 1000;
+
+int main() {
+	
+	int result = 0;
+
+	int arr [SIZE][SIZE];
+
+	for (int i =0; i<SIZE; i++) 
+		for (int j = 0 ;j < SIZE; j++) 
+		       arr[i][j] = 5;
+	/*strings*/
+	Timer t;
+	for (int i = 0; i< SIZE; i++)
+		for (int j = 0; j< SIZE; j++)
+			result+=arr[j][i];	
+
+	return result;
+}
+
