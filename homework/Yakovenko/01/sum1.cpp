f59aea45cc0855dd6df2db99b9b719f51fa682b1
meyakovenkoj <32681800+meyakovenkoj@users.noreply.github.com>
2019-03-11 17:19:54 +0300

    Adding first homework

---
+++ homework/Yakovenko/01/sum1.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+    using clock_t = std::chrono::high_resolution_clock;
+    using microseconds = std::chrono::microseconds;
+public:
+    Timer()
+    : start_(clock_t::now())
+    {
+    }
+    
+    ~Timer()
+    {
+        const auto finish = clock_t::now();
+        const auto us =
+        std::chrono::duration_cast<microseconds>
+        (finish - start_).count();
+        std::cout << us << " us" << std::endl;
+    }
+    
+private:
+    const clock_t::time_point start_;
+};
+
+int main(){
+	int size = 1000;
+	int array[size][size];
+
+	for(int i = 0; i < size; i++)
+		for(int j = 0; j < size; j++)
+			array[i][j] = 3;
+	
+	Timer t;
+	
+	int res = 0;
+	
+	for(int i = 0; i < size; i++)
+		for(int j = 0; j < size; j++)
+			res += array[i][j];
+	return res;
+}
