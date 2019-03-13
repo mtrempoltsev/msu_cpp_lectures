551a6d2ba656cb2e650d2a8c0106412ea84b9a59
Ermolovskii <ermolovskii.nikita@mail.ru>
2019-03-13 15:49:35 +0300

    homework added

---
+++ homework/ErmolovskyNikita/01/01a/cmake-build-debug/CMakeFiles/1a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1a.dir/main.cpp.obj"
+  "1a.pdb"
+  "1a.exe"
+  "1a.exe.manifest"
+  "lib1a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework/ErmolovskyNikita/01/01a/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+
+class Timer
+{
+    using clock_t = std::chrono::high_resolution_clock;
+    using microseconds = std::chrono::microseconds;
+public:
+    Timer()
+            : start_(clock_t::now())
+    {
+    }
+
+    ~Timer()
+    {
+        const auto finish = clock_t::now();
+        const auto us =
+                std::chrono::duration_cast<microseconds>
+                        (finish - start_).count();
+        std::cout << us << " us" << std::endl;
+    }
+
+private:
+    const clock_t::time_point start_;
+};
+using namespace std;
+int main() {
+    int n;
+    n = 500;
+    int arr[n][n];
+    long long int sum = 0;
+    Timer t;
+    for (int i = 0; i < n; i++){
+        for (int k = 0; k < n; k++){
+            arr[k][i] = i+k+1;
+            sum+=arr[i][k];
+        }
+    }
+    cout << sum;
+
+    return 0;
+}
